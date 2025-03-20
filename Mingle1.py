38b6ec94a0da40d974407f782d114381c72a50be
julie2702 <julie.rampasova@icloud.com>
2025-03-20 01:52:58 -0700

    zkouska

--- Mingle1.py
+++ Mingle1.py
@@ -9,9 +9,6 @@ canvas = tk.Canvas(root, width=300, height=300, bg="white")
 canvas.pack()
 
 # Nakreslení srdce pomocí dvou kruhů a trojúhelníku
-canvas.create_oval(50, 50, 150, 150, fill="red", outline="red")  # Levý kruh
-canvas.create_oval(150, 50, 250, 150, fill="red", outline="red")  # Pravý kruh
-canvas.create_polygon(50, 100, 250, 100, 150, 250, fill="red", outline="red")  # Spodní část
 
 # Spuštění aplikace
 root.mainloop()
